667c9d8da5018a5aa61d046d0e5a940256dbfd24
Charan Y <118613759+racha-coder@users.noreply.github.com>
2023-11-10 19:30:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const container = document.querySelector('.container');
 document.addEventListener('mousemove', (e) => {
     tiltEffect(e, container);
 });
+document.addEventListener('mouseon', (e) => {
+    tiltEffect(e, container);
+});
 
 function tiltEffect(event, element) {
     // Get the position of the cursor / mouse
@@ -15,10 +18,20 @@ function tiltEffect(event, element) {
     const middleY = window.innerHeight / 2;
 
     // Finding the offset from the middle
-    const offsetX = ((x - middleX) / middleX) * 40;
-    const offsetY = ((y - middleY) / middleY) * 40;
+    const offsetX = ((x - middleX) / middleX) * 50;
+    const offsetY = ((y - middleY) / middleY) * 50;
+
+    // Calculating the shadow effect
+    const shadowX = offsetX / 50;
+    const shadowY = offsetY / 50;
+
 
     // Updating the elements rotation
     element.style.setProperty("--rotateX", -offsetY + "deg");
     element.style.setProperty("--rotateY", offsetX + "deg");
+
+    
+    element.style.setProperty("--translate-shadowX", shadowX + "rem");
+    element.style.setProperty("--translate-shadowY", shadowY + "rem");
+
 }
